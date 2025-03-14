cca8e466c6c6ce0a99088ff96f6979afaff594d9
lab1456-dev <lab1456+github@gmail.com>
2025-03-14 01:13:39 -0400

    CORS testing

--- src/Main.tsx
+++ src/Main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from "./contexts/AuthProvider";
 import App from "./App";
-import 'virtual:windi.css'; // Import Windi CSS  
+import 'virtual:windi.css'; // Import Windi CSS    
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
